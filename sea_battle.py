378b1b511e913b3e80fa95e473a2c4208cf35832
AntonRakov <62613158+AntRakov@users.noreply.github.com>
2023-03-15 19:17:22 +0300

    Update sea_battle.py
    change size block

--- sea_battle.py
+++ sea_battle.py
@@ -6,7 +6,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 # Переменные для масштабации основного экрана
-block_size = 50
+block_size = 40
 left_margin = 100
 upper_margin = 80
 size = (left_margin + 30 * block_size , upper_margin + 15 * block_size)
